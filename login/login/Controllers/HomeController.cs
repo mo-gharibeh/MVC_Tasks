c9995abb91f90ddc7d0be9c6fcf29a166626640e
hmoodeh98 <hmoodeh9820@gmail.com>
2024-07-25 02:33:25 +0300

    24-7-2024

---
+++ login/login/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace login.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["Loggedin"] = "0";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact(FormCollection form)
+        {
+            
+            string email = form["email"];
+            string city = form["selectcity"];            
+            string feedback = form["feedback"];
+
+            ViewBag.Email = email;
+            ViewBag.City = city;
+            ViewBag.Feedback = feedback;
+            
+
+
+            return View();
+        }
+    }
+}
---
+++ login/login/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace login.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginPage(FormCollection bass)
+        {
+            string name = bass["name"];
+            string email = bass["email"];
+            string password = bass["password"];
+
+            string[] arr = { "mohammad@gmail.com", "123456aa" };
+
+            
+            if (email == arr[0] && password == arr[1])
+            {
+
+                Session["Loggedin"] = "1";
+                return RedirectToAction("Index", "Home");
+            }
+            else 
+            {
+                Session["Loggedin"] = "0";
+                return View();
+                
+            }
+          
+
+        }
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
