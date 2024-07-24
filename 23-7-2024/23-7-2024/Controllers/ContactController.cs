56e31b7033b5f15488d686091413047b329d86ff
hmoodeh98 <hmoodeh9820@gmail.com>
2024-07-24 03:57:48 +0300

    23-7-2024

---
+++ 23-7-2024/23-7-2024/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23_7_2024.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(FormCollection form)
+        {
+            var text = form["name"];
+            var number = form["age"];
+            var radioOption = form["gender"];
+            var selectOption = form["select"];
+            var multiSelectOptions = form["MultiSelectOptions"];
+
+            ViewBag.name = text;
+            ViewBag.age = number;
+            ViewBag.gender = radioOption;
+            ViewBag.select = selectOption;
+            ViewBag.MultiSelectOptions = multiSelectOptions;
+
+            return View("ContactResult");
+        }
+        public ActionResult ContactResult()
+        {
+            return View();
+        }
+    }
+}
