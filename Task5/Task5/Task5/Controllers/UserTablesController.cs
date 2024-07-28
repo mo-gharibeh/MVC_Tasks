d1ad14092888ba0a743cfd969c4d4371ad52e21b
hmoodeh98 <hmoodeh9820@gmail.com>
2024-07-29 00:08:18 +0300

    28-7-2024

---
+++ Task5/Task5/Task5/Controllers/UserTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Task5.Models;
+
+namespace Task5.Controllers
+{
+    public class UserTablesController : Controller
+    {
+        private DBFirstEntities db = new DBFirstEntities();
+
+        // GET: UserTables
+        public ActionResult Index()
+        {
+            return View(db.UserTables.ToList());
+        }
+
+        // GET: UserTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // GET: UserTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserTables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Emaile,Password,Image")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTables.Add(userTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userTable);
+        }
+
+        // GET: UserTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: UserTables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Emaile,Password,Image")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userTable);
+        }
+
+        // GET: UserTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: UserTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserTable userTable = db.UserTables.Find(id);
+            db.UserTables.Remove(userTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
